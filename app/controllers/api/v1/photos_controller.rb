aba0160997a5774bcbf14ffd7d041669d8676cc2
Joan Ruiz <joan.ruiz.e@gmail.com>
2015-04-02 10:52:41 -0500

    close method

--- app/controllers/api/v1/photos_controller.rb
+++ app/controllers/api/v1/photos_controller.rb
@@ -10,6 +10,7 @@ class Api::V1::PhotosController < ApplicationController
   def show
     @photo = Photo.find(params[:id])
     render json: @photo
+  end
 
   def create
     @photo = Photos.new(photos_params)
