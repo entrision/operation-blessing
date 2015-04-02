a7e602b42865ee73cf533cd3b8be15f5704e7634
Joan Ruiz <joan.ruiz.e@gmail.com>
2015-04-02 13:09:20 -0500

    Add date filter

--- app/controllers/api/v1/photos_controller.rb
+++ app/controllers/api/v1/photos_controller.rb
@@ -3,7 +3,15 @@ class Api::V1::PhotosController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def index
-    @photos = Photo.sort_by_date
+    if params[:date]
+      begin
+        @photos = Photo.where(for_date: Date.parse(params[:date])).sort_by_date
+      rescue ArgumentError => e
+        @photos = Photo.sort_by_date
+      end
+    else
+      @photos = Photo.sort_by_date
+    end
     render json: @photos.as_json
   end
 
