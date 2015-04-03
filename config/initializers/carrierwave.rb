07bfc47e7c0c7c44a704347caf2ab24cf8acb002
Joan Ruiz <joan.ruiz.e@gmail.com>
2015-04-03 14:07:54 -0500

    Add environment variables for heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,10 +2,10 @@ CarrierWave.configure do |config|
   config.storage = :fog
   config.fog_credentials = {
     :provider              => 'AWS',
-    :aws_access_key_id     => Rails.application.secrets.s3_key,
-    :aws_secret_access_key => Rails.application.secrets.s3_secret_key
+    :aws_access_key_id     => Rails.application.secrets.s3_key || ENV['S3_KEY'],
+    :aws_secret_access_key => Rails.application.secrets.s3_secret_key || ENV['S3_SECRET_KEY']
   }
-  config.fog_directory    = Rails.application.secrets.s3_bucket
+  config.fog_directory    = Rails.application.secrets.s3_bucket || ENV['S3_BUCKET']
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
 
   if Rails.env.test?
